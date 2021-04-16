f9bab3b16caa475009801e466a0e362a210c6155
Nurki Putra Mahardika <nurput28@gmail.com>
2021-04-16 20:33:11 +0700

    Exercise

---
+++ exercise 7.33.py
@@ -0,0 +1,7 @@
+a = [10,40,30,10,70,40]
+print("a awal= ", a)
+#menghapus 10 dari list
+a.pop()
+#menghapus 40 dari list
+a.pop()
+print("hasil= ",a)
---
+++ exercise 7.4.py
@@ -0,0 +1,9 @@
+def panggil (func):
+    return func
+def helloworld():
+    return "HELLO WORD"
+def main():
+    s = panggil(helloword())
+    print(s)
+    if __name__  == '__main__':
+        main()
